0f6735e1f4dd43d4fe2bd3b2edce661717ab513e
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-17 18:30:31 -0500

    Initial Commit

---
+++ DailyWorkout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyWorkout
+//
+//  Created by Joseph DeWeese on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyWorkout/DailyWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWorkoutApp.swift
+//  DailyWorkout
+//
+//  Created by Joseph DeWeese on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
